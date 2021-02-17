dc1ac627b20220fc6b7e42c49fb26a7ce323f803
gianpierregalvez <gianpierre.galvez@wizeline.com>
2021-02-17 13:03:10 -0500

    Show port

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require("dotenv/config");
 const appConfig = require("./config");
 const app = require("./src/app");
-app.listen(appConfig.port);
+
+const listener = app.listen(appConfig.port, () => {
+	console.log(`Listening on http://localhost:${listener.address().port}`);
+});
