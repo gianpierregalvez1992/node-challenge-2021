d57fa569466e40ded40765cd718fed7f20353475
gianpierregalvez <gianpierre.galvez@wizeline.com>
2021-02-18 08:06:14 -0500

    Delete testing routes

--- router.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const videosRoute = require("./src/resources/videos/videos.router");
-
-router.use("/videos", videosRoute);
-module.exports.router = router;
